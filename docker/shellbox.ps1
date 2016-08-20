14f134241967dfb04b85bcff2b2e01dd6a2e6eb9
Kaspars Mickevics <kaspars@fx.lv>
2016-08-20 12:33:51 +0300

    Work on both Windows and OSX

--- docker/shellbox.ps1
+++ docker/shellbox.ps1
@@ -1,1 +1,21 @@
-docker run -ti fxlv/debian_wheezy:shellbox bash
+# Run a shellbox container
+# mount resources directory from OneDrive and do it on both windows and osx
+
+$localResourcesPath = false
+$userName = $(whoami)
+
+if (Test-Path /usr/bin/uname) {
+    # this is likely unix
+    # do stuff
+    Write-Output "Running on OSX"
+    $localResourcesPath = "/Users/$userName/OneDrive/opt/docker-scripts/resources"
+
+} else {
+    # this is likely windows
+    Write-Output "Running on Windows"
+    $localResourcesPath = "/c/Users/$userName/OneDrive/opt/docker-scripts/resources"
+}
+
+$cmd = "docker run -v $($localResourcesPath):/resources -t -i fxlv/debian_wheezy:shellbox bash"
+
+Invoke-Expression $cmd

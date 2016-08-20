ae97a0b6bf8a082c6a24aa468acbdbc29e787b1c
Kaspars Mickevics <kaspars@fx.lv>
2016-08-20 12:13:10 +0300

    Run on windows only

--- docker/docker-env.ps1
+++ docker/docker-env.ps1
@@ -1,6 +1,11 @@
+if ( Test-Path /usr/bin/uname ){
+    write-host "This script is meant for windows only"
+    exit
+}
+
 if ( docker-machine ls | select-string -quiet -pattern dev11 ) {
-	write-host "Docker machine is up, let me set it up for you."
-	docker-machine.exe env --shell powershell dev11 | Invoke-Expression
+    write-host "Docker machine is up, let me set it up for you."
+    docker-machine.exe env --shell powershell dev11 | Invoke-Expression
 } else {
-	echo "Docker machine is down."
+    echo "Docker machine is down."
 }
